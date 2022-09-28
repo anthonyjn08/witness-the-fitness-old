ddc1d0a91f472f3d08d5a6d5c42a6b300121a282
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-09-28 21:24:08 +0000

    Add sport image to classes model

--- classes/models.py
+++ classes/models.py
@@ -6,6 +6,7 @@ class Classes(models.Model):
     sport = models.CharField(max_length=100, null=False, blank=False)
     cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
     location = models.CharField(max_length=100, null=False, blank=False)
+    sport_image = models.ImageField(default='placeholder')
 
     def __str__(self):
         return self.sport
